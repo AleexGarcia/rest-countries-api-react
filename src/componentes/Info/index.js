9dbc8354c5192e12127777537a4f687ed25641f4
Alexandre Garcia <97323322+AleexGarcia@users.noreply.github.com>
2022-09-14 21:37:40 -0300

    feat: info

--- src/componentes/Info/index.js
+++ src/componentes/Info/index.js
@@ -1,54 +1,52 @@
-import Api from '../../api_teste'
-export default function Info() {
+
+export default function Info(props, id) {
     return (
-        Api.map(card => (
-            <>
-                <button id="more-info__btn" class="more-info__btn">Back</button>
-                <figure id="country" class="country">
-                    <img src={card.flags.svg} alt={card.name} class="country__img" />
-                    <figcaption class="country__info">
-                        <h1 class="country__name">${card.name}</h1>
-                        <div class="info-1">
-                            <span class="country__name-native" ><strong>Native Name: </strong>{card.nativeName
-                            }
-                            </span>
-                            <span class="country__population"><strong>Population: </strong>{card.population}</span>
-                            <span class="country__region"><strong>Region: </strong>{card.region}</span>
-                            <span class="country__sub-region"><strong>Sub Region: </strong>{card.subregion}</span>
-                            <span class="country__capital"><strong>Capital: </strong>{card.capital}</span>
-                        </div>
-                        <div class="info-2">
-                            <span class="country__domain"><strong>Top Level Domain: </strong>{card.topLevelDomain}</span>
-                            <span class="country__currencies"><strong>Currencies: </strong>{card.currencies[0].name}</span>
-                            <span class="country__languages"><strong>Languages: </strong>{card.languages.map(e => {
-                                return `${e.name} `
-                            })}</span>
-                        </div>
-                        <div class="info__border">
-                            <span class="country__border">Border Countries</span>
-                            <div class="border__links">
-                                {card.borders != null ? card.borders.map(sigla => {
-                                    for (let i = 0; i < ListaDeCards.cards.length; i++) {
-                                        if (sigla == ListaDeCards.cards[i].alpha3Code) {
-                                            return `
-                             <a onclick='ExibeMoreInfo(${i})' class="country__border-country pointer">${ListaDeCards.cards[i].name}</a>
-                        
-                            `
-                                        }
+
+        <>
+            <button id="more-info__btn" class="more-info__btn">Back</button>
+            <figure id="country" class="country">
+                <img src={props.flags.svg} alt={props.name} class="country__img" />
+                <figcaption class="country__info">
+                    <h1 class="country__name">${props.name}</h1>
+                    <div class="info-1">
+                        <span class="country__name-native" ><strong>Native Name: </strong>{props.nativeName
+                        }
+                        </span>
+                        <span class="country__population"><strong>Population: </strong>{props.population}</span>
+                        <span class="country__region"><strong>Region: </strong>{props.region}</span>
+                        <span class="country__sub-region"><strong>Sub Region: </strong>{props.subregion}</span>
+                        <span class="country__capital"><strong>Capital: </strong>{props.capital}</span>
+                    </div>
+                    <div class="info-2">
+                        <span class="country__domain"><strong>Top Level Domain: </strong>{props.topLevelDomain}</span>
+                        <span class="country__currencies"><strong>Currencies: </strong>{props.currencies[0].name}</span>
+                        <span class="country__languages"><strong>Languages: </strong>{props.languages.map(e => {
+                            { e.name }
+                        })}</span>
+                    </div>
+                    <div class="info__border">
+                        <span class="country__border">Border Countries</span>
+                        <div class="border__links">
+                            {props.borders != null ? props.borders.map(sigla => {
+                                for (let i = 0; i < countries.length; i++) {
+                                    if (sigla == countries[i].alpha3Code) {
+                                        return
+                                        <a onClick = ExibeMoreInfo({i}) class="country__border-country pointer">${ListaDeCards.cards[i].name}</a>
                                     }
-                                }).join('')
-                                    : <span class="country__border-country">There are no border countries</span>
                                 }
+                            }).join('')
+                                : <span class="country__border-country">There are no border countries</span>
+                            }
 
-                            </div>
                         </div>
-                    </figcaption>
-                </figure>
-            </>
-        ))
-
-
-
+                    </div>
+                </figcaption>
+            </figure>
+        </>
     )
+
+
+
+
 }
 
